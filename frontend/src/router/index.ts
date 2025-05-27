b7f8c74911f5f45369465f42e55614aee66c36b4
ryma nait amara <amarcharacha@gmail.com>
2025-05-27 15:05:42 +0200

    add and clean up frontend project folder and install vue latest

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import WeatherPage from '../views/WeatherPage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: WeatherPage,
+    },
+  ],
+})
+
+export default router
---
+++ frontend/src/views/WeatherPage.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>bienvenue dans adcleek weather app</main>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped></style>
